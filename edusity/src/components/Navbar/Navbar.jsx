d40f1e8e20f5577bc0cb52f0a5923c0675e02b76
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-22 10:48:43 +0530

    added navbar

---
+++ edusity/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="container">
+      <img src={logo} alt="logo" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li>
+          <button className="btn">Contact us</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
