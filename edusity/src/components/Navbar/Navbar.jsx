e8e13a9668019eed08df94c9823e32bac2c46d73
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-22 14:26:22 +0530

    application responsive

--- edusity/src/components/Navbar/Navbar.jsx
+++ edusity/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import menu_icon from "../../assets/menu-icon.png";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
@@ -12,11 +13,14 @@ const Navbar = () => {
       window.scrollY > 500 ? setSticky(true) : setSticky(false);
     });
   }, []);
-
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""} `}>
       <img src={logo} alt="logo" className="logo" />
-      <ul>
+      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>
           <Link to="hero" smooth={true} offset={0} duration={500}>
             Home
@@ -54,6 +58,7 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 };
