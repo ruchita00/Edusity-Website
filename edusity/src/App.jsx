e90faccd538a45ca2a435957dfa3c1556da83705
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-22 12:57:30 +0530

    implemented testimonials sliders

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -3,6 +3,7 @@ import Campus from "./components/Campus/Campus";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Programs from "./components/Programs/Programs";
+import Testimonials from "./components/Testimonials/Testimonials";
 import Title from "./components/Title/Title";
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
         <Title subTitle={"Gallery"} title={"Campus Photos"} />
         <Campus />
         <Title subTitle={"TESTIMONIALS"} title={"What Students Says"} />
+        <Testimonials />
       </div>
     </div>
   );
---
+++ edusity/src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,117 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useRef } from "react";
+import "./Testimonials.css";
+import next_icon from "../../assets/next-icon.png";
+import back_icon from "../../assets/back-icon.png";
+import user_1 from "../../assets/user-1.png";
+import user_2 from "../../assets/user-2.png";
+import user_3 from "../../assets/user-3.png";
+import user_4 from "../../assets/user-4.png";
+
+const Testimonials = () => {
+  const slider = useRef();
+  let tx = 0;
+
+  const slideForward = () => {
+    if (tx > -50) {
+      tx -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+  const slideBackward = () => {
+    if (tx < 0) {
+      tx += 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+  return (
+    <div className="testimonials">
+      <img
+        src={next_icon}
+        alt="next-icon"
+        className="next-btn"
+        onClick={slideForward}
+      />
+      <img
+        onClick={slideBackward}
+        src={back_icon}
+        alt="back-icon"
+        className="back-btn"
+      />
+      <div className="slider">
+        <ul ref={slider}>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="user-1" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Choosing to purse my degree at edusity was one of the best
+                decision I have ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have trully exceeded my expectations.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_2} alt="user-2" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Choosing to purse my degree at edusity was one of the best
+                decision I have ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have trully exceeded my expectations.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_3} alt="user-3" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Choosing to purse my degree at edusity was one of the best
+                decision I have ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have trully exceeded my expectations.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="user-4" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>
+                Choosing to purse my degree at edusity was one of the best
+                decision I have ever made. The supportive community,
+                state-of-the-art facilities, and commitment to academic
+                excellence have trully exceeded my expectations.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
